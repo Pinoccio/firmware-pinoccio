23d97035170f599d534b04f4281fb224d3cfc18f
Matthijs Kooijman <matthijs@stdin.nl>
2014-02-26 09:56:50 +0100

    Let update.sh check the git version
    Git 1.8.2 introduced support for remote fetches, which we need.

--- update.sh
+++ update.sh
@@ -1,5 +1,22 @@
 #/bin/sh -e
 
+# return 0 if program version is equal or greater than minimum version
+# based on http://fitnr.com/bash-comparing-version-strings.html
+check_version()
+{
+    version=$1 minimum=$2
+    winner=$( (echo "$version"; echo "$minimum") | sort --version-sort --reverse | head -1)
+    [ "$winner" = "$version" ] && return 0
+    return 1
+}
+
+GIT_VERSION=$(git --version | sed 's/^[^.0-9]*//')
+GIT_REQUIRED=1.8.4
+if ! check_version "$GIT_VERSION" "$GIT_REQUIRED"; then
+	echo "Git version too old. Found: $GIT_VERSION. Required: $GIT_REQUIRED"
+	exit 1;
+fi
+
 echo "Updating..."
 
 # Resync submodule urls in case .submodules changed (.git/config
